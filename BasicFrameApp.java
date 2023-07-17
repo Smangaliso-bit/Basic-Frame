b7b4d7370007cd7e2e7fa7696fa4860d23294691
Smangaliso57 <139795321+Smangaliso57@users.noreply.github.com>
2023-07-17 18:38:23 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,13 @@
+package basicframeapp;
+
+import za.ac.tut.ul.MyFirstFrame;
+
+public class BasicFrameApp {
+
+    public static void main(String[] args) {
+        
+        MyFirstFrame  MyFrame = new MyFirstFrame();
+        
+    }
+    
+}
